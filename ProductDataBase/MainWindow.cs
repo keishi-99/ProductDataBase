273bc0aee6461c71817ad64ab3b3f7b30598c8a0
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2024-11-18 12:55:57 +0900

    履歴検索のクエリ修正

--- ProductDataBase/MainWindow.cs
+++ ProductDataBase/MainWindow.cs
@@ -263,12 +263,14 @@ namespace ProductDatabase {
                             ProductInfo.ProductName = selectedRow[0]["ProductName"].ToString() ?? string.Empty;
                             ProductInfo.SubstrateName = selectedRow[0]["SubstrateName"].ToString() ?? string.Empty;
                             ProductInfo.SubstrateModel = selectedRow[0]["SubstrateModel"].ToString() ?? string.Empty;
+                            ProductInfo.RegType = Convert.ToInt32(selectedRow[0]["RegType"] ?? throw new Exception("RegType is null"));
                             break;
                         case 2:
                         case 3:
                             ProductInfo.ProductName = selectedRow[0]["ProductName"].ToString() ?? string.Empty;
                             ProductInfo.ProductType = selectedRow[0]["ProductType"].ToString() ?? string.Empty;
                             ProductInfo.ProductModel = selectedRow[0]["ProductModel"].ToString() ?? string.Empty;
+                            ProductInfo.RegType = Convert.ToInt32(selectedRow[0]["RegType"] ?? throw new Exception("RegType is null"));
                             break;
                     }
 
