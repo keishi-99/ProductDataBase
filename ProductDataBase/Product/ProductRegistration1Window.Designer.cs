72f9fe11f399df46b54bffe714489f365c3218c8
Keishi Tokudome <Gideon_apple@yahoo.co.jp>
2024-12-09 16:04:11 +0900

    製造番号のアルファベットを強制的に大文字に

--- ProductDataBase/Product/ProductRegistration1Window.Designer.cs
+++ ProductDataBase/Product/ProductRegistration1Window.Designer.cs
@@ -394,7 +394,7 @@
             // 
             this.ManufacturingNumberMaskedTextBox.Enabled = false;
             this.ManufacturingNumberMaskedTextBox.Location = new Point(19, 191);
-            this.ManufacturingNumberMaskedTextBox.Mask = "LA00L00000-0000";
+            this.ManufacturingNumberMaskedTextBox.Mask = ">LA00L00000-0000";
             this.ManufacturingNumberMaskedTextBox.Name = "ManufacturingNumberMaskedTextBox";
             this.ManufacturingNumberMaskedTextBox.PromptChar = '*';
             this.ManufacturingNumberMaskedTextBox.Size = new Size(120, 23);
