959029afc5f7083245c6b8eb512469cdc680447d
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2025-02-12 15:50:28 +0900

    再度修正

--- ProductDataBase/Other/HistoryWindow.cs
+++ ProductDataBase/Other/HistoryWindow.cs
@@ -425,7 +425,7 @@ namespace ProductDatabase {
                 var searchAddressResultRow = searchAddressResult.Start.Row;
 
                 // ワークシートのセルから値を取得
-                var directoryPath = workSheetMain.Cells[searchAddressResultRow, 3].Value.ToString();
+                var directoryPath = workSheetMain.Cells[searchAddressResultRow, 3].Value?.ToString()?.Trim('"');
                 if (string.IsNullOrWhiteSpace(directoryPath)) { throw new Exception("Configのファイルパスが無効です。"); }
                 if (!Directory.Exists(directoryPath)) { throw new FileNotFoundException($"指定されたフォルダが存在しません: {directoryPath}"); }
 
