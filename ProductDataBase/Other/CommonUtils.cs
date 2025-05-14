f4435f48453aba8edef839f8d16fe8ffde69c4cc
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2025-05-14 15:29:35 +0900

    成績書生成 仕様変更

--- ProductDataBase/Other/CommonUtils.cs
+++ ProductDataBase/Other/CommonUtils.cs
@@ -148,11 +148,11 @@ namespace ProductDatabase.Other {
                 var sheetName = !string.IsNullOrEmpty(workSheetMain.Cells[searchAddressResultRow, 5].Value.ToString())
                     ? workSheetMain.Cells[searchAddressResultRow, 5].Value.ToString()
                     : throw new Exception("シート名がありません。");
-                var productNumberRange = workSheetMain.Cells[searchAddressResultRow, 6].Value?.ToString() ?? workSheetMain.Cells[2, 6].Value.ToString();
-                var orderNumberRange = workSheetMain.Cells[searchAddressResultRow, 7].Value?.ToString() ?? workSheetMain.Cells[2, 7].Value.ToString();
-                var quantityRange = workSheetMain.Cells[searchAddressResultRow, 8].Value?.ToString() ?? workSheetMain.Cells[2, 8].Value.ToString();
-                var serialFirstRange = workSheetMain.Cells[searchAddressResultRow, 9].Value?.ToString() ?? workSheetMain.Cells[2, 9].Value.ToString();
-                var serialLastRange = workSheetMain.Cells[searchAddressResultRow, 10].Value?.ToString() ?? workSheetMain.Cells[2, 10].Value.ToString();
+                var productNumberRange = workSheetMain.Cells[searchAddressResultRow, 6].Value?.ToString();
+                var orderNumberRange = workSheetMain.Cells[searchAddressResultRow, 7].Value?.ToString();
+                var quantityRange = workSheetMain.Cells[searchAddressResultRow, 8].Value?.ToString();
+                var serialFirstRange = workSheetMain.Cells[searchAddressResultRow, 9].Value?.ToString();
+                var serialLastRange = workSheetMain.Cells[searchAddressResultRow, 10].Value?.ToString();
                 var productModelRange = workSheetMain.Cells[searchAddressResultRow, 11].Value?.ToString();
                 var saveDirectory = workSheetMain.Cells[searchAddressResultRow, 12].Value?.ToString();
                 using FileStream fileStreamReport = new(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
@@ -161,7 +161,7 @@ namespace ProductDatabase.Other {
 
                 // セルに値を挿入
                 var workSheetTemp = reportSheet[sheetName];
-                if (!string.IsNullOrEmpty(productInfo.ProductNumber)) {
+                if (!string.IsNullOrEmpty(productNumberRange) && !string.IsNullOrEmpty(productInfo.ProductNumber)) {
                     var productNumbers = productInfo.ProductNumber.Split("-");
                     workSheetTemp.Cells[productNumberRange].Value = productNumbers[0];
                 }
