3fdbd42c8268faa4d36b7fa30e8375fdcadc187c
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2025-10-24 14:26:34 +0900

    銘版印刷処理 修正

--- ProductDataBase/Other/RePrintWindow.cs
+++ ProductDataBase/Other/RePrintWindow.cs
@@ -380,6 +380,7 @@ namespace ProductDatabase {
             var outputCode = _serialType switch {
                 "Label" => LabelPrintSettings.TextFormat ?? string.Empty,
                 "Barcode" => BarcodePrintSettings.TextFormat ?? string.Empty,
+                "Nameplate" => NameplatePrintSettings.TextFormat ?? string.Empty,
                 _ => string.Empty
             };
 
