06237a6434f93b8ff7ef5237cab6ff141dabec9c
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2025-12-18 11:11:26 +0900

    スプラッシュスクリーン変更

--- Launcher/SplashForm.Designer.cs
+++ Launcher/SplashForm.Designer.cs
@@ -24,17 +24,43 @@
         /// </summary>
         private void InitializeComponent() {
             var resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashForm));
+            this.label1 = new Label();
+            this.pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)this.pictureBox1).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = Color.RoyalBlue;
+            this.label1.Font = new Font("Meiryo UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 128);
+            this.label1.ForeColor = Color.Transparent;
+            this.label1.Location = new Point(36, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(228, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " DataBase ";
+            this.label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            this.pictureBox1.Location = new Point(22, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new Size(256, 256);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // SplashForm
             // 
             this.AutoScaleDimensions = new SizeF(7F, 15F);
             this.AutoScaleMode = AutoScaleMode.Font;
-            this.BackColor = Color.Green;
-            this.BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            this.BackgroundImageLayout = ImageLayout.Zoom;
-            this.ClientSize = new Size(400, 400);
+            this.BackColor = SystemColors.Control;
+            this.BackgroundImageLayout = ImageLayout.Center;
+            this.ClientSize = new Size(300, 350);
             this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = FormBorderStyle.None;
             this.Icon = (Icon)resources.GetObject("$this.Icon");
@@ -43,10 +69,15 @@
             this.StartPosition = FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
             this.TopMost = true;
-            this.TransparencyKey = Color.Green;
+            this.Load += this.SplashForm_Load;
+            ((System.ComponentModel.ISupportInitialize)this.pictureBox1).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
     }
 }
--- Launcher/SplashForm.cs
+++ Launcher/SplashForm.cs
@@ -11,5 +11,29 @@ namespace Launcher {
         public SplashForm() {
             InitializeComponent();
         }
+
+        private void SplashForm_Load(object sender, EventArgs e) {
+
+            int radius = 20;
+            int diameter = radius * 2;
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+
+            // 左上
+            gp.AddPie(0, 0, diameter, diameter, 180, 90);
+            // 右上
+            gp.AddPie(this.Width - diameter, 0, diameter, diameter, 270, 90);
+            // 左下
+            gp.AddPie(0, this.Height - diameter, diameter, diameter, 90, 90);
+            // 右下
+            gp.AddPie(this.Width - diameter, this.Height - diameter, diameter, diameter, 0, 90);
+            // 中央
+            gp.AddRectangle(new Rectangle(radius, 0, this.Width - diameter, this.Height));
+            // 左
+            gp.AddRectangle(new Rectangle(0, radius, radius, this.Height - diameter));
+            // 右
+            gp.AddRectangle(new Rectangle(this.Width - radius, radius, radius, this.Height - diameter));
+
+            this.Region = new Region(gp);
+        }
     }
 }
