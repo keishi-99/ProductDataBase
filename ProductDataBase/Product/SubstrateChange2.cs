53cefc18562d3332c96eb047ee157d81c5601b02
Keishi Tokudome <Gideon_apple@yahoo.co.jp>
2026-01-27 08:59:27 +0900

    ログ出力 修正

--- ProductDataBase/Product/SubstrateChange2.cs
+++ ProductDataBase/Product/SubstrateChange2.cs
@@ -508,6 +508,7 @@ namespace ProductDatabase {
                     $"ID[{_productRegisterWork.RowID}]",
                     $"注文番号[{_productRegisterWork.OrderNumber}]",
                     $"製造番号[{_productRegisterWork.ProductNumber}]",
+                    $"[]",
                     $"製品名[{_productMaster.ProductName}]",
                     $"タイプ[{_productMaster.ProductType}]",
                     $"型式[{_productMaster.ProductModel}]",
