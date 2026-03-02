3d74151f9c720db1fee01036a23046a95dbe920a
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2026-03-03 08:59:35 +0900

    登録必須項目はチェックを外せないように変更

--- ProductDataBase/Product/ProductRegistration1Window.Designer.cs
+++ ProductDataBase/Product/ProductRegistration1Window.Designer.cs
@@ -156,6 +156,7 @@
             // 
             // RegistrationDateCheckBox
             // 
+            this.RegistrationDateCheckBox.AutoCheck = false;
             this.RegistrationDateCheckBox.Location = new Point(166, 268);
             this.RegistrationDateCheckBox.Margin = new Padding(0);
             this.RegistrationDateCheckBox.Name = "RegistrationDateCheckBox";
@@ -179,6 +180,7 @@
             // 
             // FirstSerialNumberCheckBox
             // 
+            this.FirstSerialNumberCheckBox.AutoCheck = false;
             this.FirstSerialNumberCheckBox.Location = new Point(19, 268);
             this.FirstSerialNumberCheckBox.Margin = new Padding(0);
             this.FirstSerialNumberCheckBox.Name = "FirstSerialNumberCheckBox";
@@ -295,6 +297,7 @@
             // 
             // QuantityCheckBox
             // 
+            this.QuantityCheckBox.AutoCheck = false;
             this.QuantityCheckBox.Location = new Point(19, 218);
             this.QuantityCheckBox.Margin = new Padding(0);
             this.QuantityCheckBox.Name = "QuantityCheckBox";
